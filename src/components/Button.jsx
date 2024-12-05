0b9daf46b316313722b335e2381598a61a35a88d
jigycode <jigyasha040106@gmail.com>
2024-12-05 23:55:45 +0530

    code initiated

---
+++ src/components/Button.jsx
@@ -0,0 +1,24 @@
+import ButtonSvg from '../assets/svg/ButtonSvg'
+
+const Button =({ className, herf, onClick, children, px, white}) => {
+    const clases =`button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${px || 'px-7'} ${white ? 'text-n-8' : 'text-n-1'} ${className || ''}`
+     const spanClasses = 'relative z-10'
+  const renderButton =() => (
+    <button className={clases}>
+        <span className='spanClasses'>{children}</span>
+        {ButtonSvg(white)}
+    </button>
+  )
+
+   const renderLink =() => (
+    <a href={herf} className={clases}> <span className='spanClasses'>{children}</span>
+    {ButtonSvg(white)}
+    </a>
+   )
+
+  return herf
+  ? renderLink()
+  : renderButton
+}
+
+export default Button
